842d2b0a2ddaac2c7c3d923ed802d5a7fbddcc50
Jan Fejtek <jan.fejtek@gmail.com>
2017-10-28 20:21:46 +0000

    done

--- server.js
+++ server.js
@@ -1,20 +1,44 @@
 var express = require('express');
-
+var GoogleImages = require('google-images');
+const client = new GoogleImages(process.env.SEARCH_ID, process.env.SEARCH_API_KEY);
+const MAX_LATEST = 10;
 
 var app = express();
 
+var latest = [];
 
+function addLatest(term) {
+  var data = {term: term, timestamp: new Date().toISOString()};
+  latest.unshift(data);
+  while (latest.length > MAX_LATEST) {
+    latest.pop();
+  }
+}
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/search*", function (request, response) {
-  
+app.get("/search", function (request, response) {
+  var term = request.query.term;
+  var offset = request.query.offset;
+  if (offset == undefined)
+    offset = 0;
+  addLatest(term);
+  client.search(term, {start: offset, linkSite: "true"})
+    .then(images => {
+    var data = [];
+    console.log(images);
+    for (var idx in images) {
+      data.push({url: images[idx].url, description: images[idx].description, originalurl: images[idx].parentPage});
+    }
+    response.send(data);
+  });
   
 });
 
 app.get("/search/latest", function(request, response) {
+  response.send(latest);
 })
 
 // listen for requests :)
